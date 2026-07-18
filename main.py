3c0c758ec95738a48f168499db10ec0dbefe0937
Mrlegend9887 <technozguyzgaming@gmail.com>
2026-07-18 21:56:33 +0530

    feat: add astronaut attribute update methods

--- main.py
+++ main.py
@@ -19,8 +19,21 @@ class Astronaut:
     
     def update_name(self, new_name):
         self.name = new_name
+    
+    def update_country(self, new_country):
+        self.country = new_country
+
+    def update_height(self, new_height):
+        self.height = new_height
+
+    def update_specialization(self, new_specialization):
+        self.specialization = new_specialization
+    
 
 astronaut1 = Astronaut("AST001","Neil Armstrong","05-08-1930","USA",180,"Pilot")
 astronaut1.display_details()
 astronaut1.update_name("Kalpana Chawla")
+astronaut1.update_country("India")
+astronaut1.update_height(175)
+astronaut1.update_specialization("Commander")
 astronaut1.display_details()
