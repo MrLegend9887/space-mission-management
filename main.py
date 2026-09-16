ed3c38346ff5f89bebe83b3438a9719c472ba56b
Mrlegend9887 <technozguyzgaming@gmail.com>
2026-09-17 00:36:55 +0530

    feat: add astronaut search by ID

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Astronaut:
+
     def __init__(self, astronaut_id, name, dob, country, height, specialization):
         self.astronaut_id = astronaut_id
         self.name = name
@@ -28,12 +29,35 @@ class Astronaut:
 
     def update_specialization(self, new_specialization):
         self.specialization = new_specialization
-    
+
+class Mission:
+
+    def __init__(self, mission_id, mission_name, launch_date, destination, status):
+        self.mission_id = mission_id
+        self.mission_name = mission_name
+        self.launch_date = launch_date
+        self.destination = destination
+        self.status = status
+        self.astronauts = []
+
+    def add_astronaut(self, astronaut):
+        self.astronauts.append(astronaut)
+
+    def find_astronaut_by_id(self):
+        found = False
+        search_id = input("Enter a valid astronauts ID: ")
+        for astronaut in self.astronauts:
+            if  search_id.lower() == (astronaut.astronaut_id).lower():
+                found = True 
+                return astronaut
+        if not found:
+            print(f"No astronaut found with the ID {search_id}.")
 
 astronaut1 = Astronaut("AST001","Neil Armstrong","05-08-1930","USA",180,"Pilot")
-astronaut1.display_details()
-astronaut1.update_name("Kalpana Chawla")
-astronaut1.update_country("India")
-astronaut1.update_height(175)
-astronaut1.update_specialization("Commander")
-astronaut1.display_details()
+astronaut2 = Astronaut("AST002","Buzz Aldrin","20-01-1930","USA",178,"Pilot")
+mission1 = Mission("MSN001","Mission Mangal","12-08-2025","Mars","Completed")
+mission1.add_astronaut(astronaut1)
+mission1.add_astronaut(astronaut2)
+
+foundcandidate = mission1.find_astronaut_by_id()
+foundcandidate.display_details()
